e7f5c9fe46fc27a871ebee0e0c8718a15b848ee6
Michele-Carter <tumeke4u@gmail.com>
2024-11-10 12:45:43 +1300

    Added prevent on click action to buttons, links and socials

--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -8,7 +8,7 @@ const PricingList = () => {
       {pricing.map((item) => (
         <div
           key={item.id}
-          className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8 odd:my-4 [&>h4]:first:text-color-2 [&>h4]:even:text-color-1 [&>h4]:last:text-color-3"
+          className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8 odd:my-6 [&>h4]:first:text-color-2 [&>h4]:even:text-color-1 [&>h4]:last:text-color-3"
         >
           <h4 className="h4 mb-4">{item.title}</h4>
 
@@ -29,10 +29,15 @@ const PricingList = () => {
 
           <Button
             className="w-full mb-6"
-            href={item.price ? "/pricing" : "mailto:contact@jsmastery.pro"}
+            href={item.price ? "#" : "mailto:daniel.wilkey@gmail.com"}
             white={!!item.price}
+            onClick={(e) => {
+              if (item.price) {
+                e.preventDefault(); // Prevents scrolling to the top of page.
+              }
+            }}
           >
-            {item.price ? "Get started" : "Contact us"}
+            {item.price ? "Get Started" : "Contact us"}
           </Button>
 
           <ul>
